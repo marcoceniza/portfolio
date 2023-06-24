1a5decdbc5d421dff7d4c6f3137f981f790aa1e3
Marco Ceniza <marcoceniza15@gmail.com>
2023-06-25 07:44:30 +0800

    June 25, 2023

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,10 +9,25 @@ export default {
   data() {
     return {
       fullText: 'I am a Web Developer',
-      typedText: ''
+      typedText: '',
+      name: '',
+      email: '',
+      phone: '',
+      message: ''
     }
   },
   methods: {
+    submitMessage() {
+      Email.send({
+        SecureToken : "867522e1-30e4-4c26-8880-a1b3e30462a5",
+        To : "marcoceniza20@gmail.com",
+        From : "marcoceniza15@gmail.com",
+        Subject : this.name,
+        Body: `Name: ${this.name}<br>Email: ${this.email}<br>Phone: ${this.phone}<br>Message: ${this.message}`
+      }).then(
+        console.log('Message sent successfully!')
+      );
+    },
     typeWriter() {
       let i = 0;
       const typingInterval = setInterval(() => {
@@ -120,6 +135,57 @@ export default {
     </div>
   </div>
 
-  <Footer />
+  <!-- <Footer /> -->
+
+  <footer id="footer">
+        <div class="container ftr_wrap">
+            <div class="ftr_box">
+                <h2><span>Touch</span> Get in Touch</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <div class="row">
+                    <div class="col-sm-8">
+                        <div class="row">
+                            <div class="col-sm-12 mb-2">
+                                <input type="text" v-model="name" class="form-control" placeholder="Name *">
+                            </div>
+                            <div class="col-sm-6 mb-2">
+                                <input type="email" v-model="email" class="form-control" placeholder="Email *">
+                            </div>
+                            <div class="col-sm-6 mb-2">
+                                <input type="text" v-model="phone" class="form-control" placeholder="Phone *">
+                            </div>
+                            <div class="col-sm-12 mb-2">
+                                <textarea v-model="message" class="form-control" rows="5" placeholder="Your Message here..."></textarea>
+                            </div>
+                            <div class="col-sm-12 mb-2">
+                                <button @click="submitMessage" class="btn btn-primary">Send Message</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-sm-4">
+                        <ul>
+                            <li><span><i class="bi bi-house-door-fill"></i> Home</span> Bontores Street Villa Kalubihan Basak San Nicolas Cebu City</li>
+                            <li><span><i class="bi bi-telephone-fill"></i> Phone</span> 09667414792</li>
+                            <li><span><i class="bi bi-envelope-fill"></i> Email</span> marcoceniza15@gmail.com</li>
+                        </ul>
+                    </div> 
+                </div>
+            </div>
+        </div>
+        <div class="copyright bg-dark">
+            <div class="container">
+                <section>
+                <p>CopyRight &copy; 2023 Marco Ceniza All Rights Reserved</p>
+                </section>
+                <section>
+                    <ul>
+                        <li><a href="javascript:;"><i class="bi bi-facebook"></i></a></li>
+                        <li><a href="javascript:;"><i class="bi bi-instagram"></i></a></li>
+                        <li><a href="javascript:;"><i class="bi bi-github"></i></a></li>
+                    </ul>
+                </section>
+            </div>
+        </div>
+    </footer>
 
 </template>
